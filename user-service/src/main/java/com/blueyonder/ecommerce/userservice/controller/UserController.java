c877533e2b3d37504d372ad42af52aea6863a633
Aditi G S <aditi07gs@gmail.com>
2024-02-23 14:45:41 +0530

    Docker file, Docker compose

--- user-service/src/main/java/com/blueyonder/ecommerce/userservice/controller/UserController.java
+++ user-service/src/main/java/com/blueyonder/ecommerce/userservice/controller/UserController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+//  @PreAuthorize(hasRole('USER'))
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/v2/ecommerce/user")
